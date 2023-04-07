ea0fbc00f3db9867c7b05d5059ef11873786bdc3
Renner15032000 <129321137+Renner15032000@users.noreply.github.com>
2023-04-07 18:28:04 -0300

    Delete 02_meu_primeiro_script.py

--- 02_meu_primeiro_script.py
+++
@@ -1,7 +0,0 @@
-#importando um módulo
-import sys #utilizar esse módulo,para conseguir ler a versão do sistema operacional
-print(sys.platform) #impressão do nome do sistema operacional S.O.
-
-print(2**100) #impressão de potência de 2 elevado a 100
-x="spam!"
-print(x*8)
